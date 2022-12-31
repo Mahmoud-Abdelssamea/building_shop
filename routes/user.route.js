688f8c265940ef4b06706b873678b7768a801c6c
Mahmoud Awwad <mabdelssamea@gmail.com>
2022-12-31 23:39:16 +0200

    add authorization to delete User and update User data

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const User = require("../controller/user.controller");
+const auth = require("../middleware/auth.middleware");
+const permission = require("../middleware/permission.middleware");
 
 // create new user
 router.post("/add", User.addNewUser);
 router.delete("/delete/:id", User.deleteUser);
-router.patch("/update/:id", User.updateUser);
-router.patch("/update/status/:id", User.updatedUserStatus);
+router.patch("/update/:id", auth, permission, User.updateUser);
+router.patch("/update/status/:id", auth, permission, User.updatedUserStatus);
 router.post("/login", User.login);
 
 module.exports = router;
